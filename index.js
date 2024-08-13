7644f89f7f0aa2ed623d32e5d07761b4428dc328
javier_tome <javier.tome@fieldeas.com>
2024-08-13 15:22:13 +0200

    cors gestionado

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
-
+import cors from 'cors';
 import servicesRoutes from './routes/servicesRoutes.js';
 import { db } from './config/db.js';
 
@@ -16,6 +16,27 @@ app.use(express.json());
 
 //Conectar a la bd
 db();
+
+//Configurar CORS
+const whitelist = [process.env.FRONTEND_URL];
+
+if(process.argv[2] === '--postman') {
+    whitelist.push(undefined);
+}
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if(whitelist.includes(origin)) {
+            //Permite la conexión
+            callback(null, true);
+        } else {
+            //Bloquea la conexión
+            callback(new Error('Error de CORS'));
+        }
+    }
+};
+
+app.use(cors(corsOptions));
 
 //Definir una ruta
 app.use('/api/services', servicesRoutes);
